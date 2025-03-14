b588cc1ea5448f465d8986d065f6270ec1e3af09
Ivo <ivose@users.noreply.github.com>
2025-03-14 13:22:48 +0200

    v2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World! This is the new version.');
+    res.send('Hello, World! An another version.');
 });
 
 app.listen(port, () => {
